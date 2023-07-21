c25f757ef0b67da0227c86056be9b8fb3a4ed2e9
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-07-21 15:36:50 +0300

    upp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Form from './Form/Form';
+import style from './App.module.css';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const { contacts } = this.state;
+    const newContact = { ...data, id: nanoid() };
+    this.setState({ contacts: [...contacts, newContact] });
+  };
+  onDeleteBtn = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  changeFilter = e => {
+    this.setState({ ...this.state, filter: e.target.value });
+  };
+  render() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div className={style.container}>
+        <h1 className={style.title}>Phonebook</h1>
+        <Form onFormSubmit={this.formSubmitHandler} contacts={contacts} />
+        <h1 className={style.title}>Contacts</h1>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteBtn={this.onDeleteBtn}
+        />
+      </div>
+    );
+  }
+}
